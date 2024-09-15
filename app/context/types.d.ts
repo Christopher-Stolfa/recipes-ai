6cd140ca1a67a7dc9a1d7e22e3d2f161f41e6fcc
Chris <xcstolfax@gmail.com>
2024-09-15 16:17:18 -0700

    Add types

--- app/context/types.d.ts
+++ app/context/types.d.ts
@@ -1,4 +1,10 @@
+import { IRecipe } from "@/types";
+
 export interface ISidebarContextProps {
   isOpen: boolean;
   toggle: () => void;
+}
+
+export interface IRecipesContextProps {
+  recipes: IRecipe[];
 }
