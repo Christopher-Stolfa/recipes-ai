e9b7931bb9baa99776989838aa01f873187b7c8e
Chris <xcstolfax@gmail.com>
2024-09-15 16:15:23 -0700

    Render a searchbar and a grid of Recipe Cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,84 @@
-import RecipeForm from "@/app/components/recipe-form/recipe-form";
+"use client";
+import { Button, Input } from "antd";
 import styles from "./page.module.scss";
+import { useForm, Resolver, Controller } from "react-hook-form";
+import { useDebounceCallback, useReadLocalStorage } from "usehooks-ts";
+import { useCallback, useContext, useEffect, useState } from "react";
+import { RecipesContext } from "./context/recipes-context";
+import { IRecipe } from "@/types";
+import RecipeCard from "./components/recipe-card/recipe-card";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
-const Home = () => {
+interface IFormValues {
+  searchInput: string;
+}
+
+const Home: React.FC = () => {
+  const router = useRouter();
+  const { recipes } = useContext(RecipesContext);
+  const { control, handleSubmit, setValue, watch } = useForm<IFormValues>({
+    defaultValues: {
+      searchInput: "",
+    },
+  });
+  const [searchedRecipes, setSearchedRecipes] = useState<IRecipe[]>([]);
+
+  const watchedValue = watch("searchInput");
+
+  const handleSearch = useCallback(
+    (searchInput = "") => {
+      console.log(searchInput);
+      const filteredRecipes = recipes?.filter((recipe) =>
+        recipe?.title?.toLowerCase()?.includes(searchInput?.toLowerCase())
+      );
+      setSearchedRecipes(filteredRecipes);
+    },
+    [recipes]
+  );
+
+  const debounceSearch = useDebounceCallback(handleSearch, 300);
+
+  const onSubmit = handleSubmit(({ searchInput }) =>
+    debounceSearch(searchInput)
+  );
+
+  const handleNewRecipe = () => router.push("/create", { scroll: false });
+
+  useEffect(() => {
+    debounceSearch(watchedValue);
+  }, [watchedValue, debounceSearch, handleSubmit]);
+
   return (
     <div className={styles.container}>
-      <RecipeForm />
+      <form className={styles.searchbox} onSubmit={onSubmit}>
+        <Controller
+          name="searchInput"
+          control={control}
+          render={({ field }) => (
+            <div>
+              <label htmlFor="searchInput">Search</label>
+              <div className={styles.flexRow}>
+                <Input
+                  {...field}
+                  role="searchbox"
+                  placeholder="Search for recipes..."
+                />
+                <Button onClick={handleNewRecipe}>Create</Button>
+              </div>
+            </div>
+          )}
+        />
+      </form>
+      <div className={styles.cardGrid}>
+        {searchedRecipes?.map((recipe, i) => (
+          <RecipeCard
+            className={styles.card}
+            recipe={recipe}
+            key={`${recipe?.title}-${i}`}
+          />
+        ))}
+      </div>
     </div>
   );
 };
