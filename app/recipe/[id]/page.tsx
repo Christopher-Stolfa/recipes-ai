eb229a109be819951b2646efdec02c29af408af0
Chris <xcstolfax@gmail.com>
2025-01-21 15:12:34 -0700

    Unhide recipe image

--- app/recipe/[id]/page.tsx
+++ app/recipe/[id]/page.tsx
@@ -49,13 +49,13 @@ const Recipe = ({ params: { id } }: IRecipeProps) => {
           </p>
           <p className={styles.description}>{recipe?.description}</p>
         </div>
-        {/* <div className={styles.headerRight}>
+        <div className={styles.headerRight}>
           <Image
             className={styles.image}
             alt={recipe?.title}
             src={recipe?.imageUrl}
           />
-        </div> */}
+        </div>
       </section>
       <section>
         <h2>Ingredients</h2>
