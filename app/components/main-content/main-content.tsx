ff22865c4eb5a8d01bb82569957b3a5b0f582dc3
Chris <xcstolfax@gmail.com>
2025-01-21 15:10:36 -0700

    Menu navigation now renders in floating buttons

--- app/components/main-content/main-content.tsx
+++ app/components/main-content/main-content.tsx
@@ -1,12 +1,39 @@
 "use client";
-import Sidebar from "../sidebar/sidebar";
+import { FloatButton, Tooltip } from "antd";
 import styles from "./main-content.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { PlusOutlined, HomeFilled, DownloadOutlined } from "@ant-design/icons";
+import { useRouter } from "next/navigation";
 
-const MainContent: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <div className={styles.container}>
-    <Sidebar />
-    <main className={styles.main}>{children}</main>
-  </div>
-);
+const MainContent: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const router = useRouter();
+
+  const handleNavigate = (path: string) => router?.push(path);
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <FloatButton.Group shape="circle" className={styles.floatButtons}>
+          <Tooltip placement="right" title="Home">
+            <FloatButton
+              onClick={() => handleNavigate("/")}
+              icon={<HomeFilled />}
+            />
+          </Tooltip>
+          <Tooltip placement="right" title="Create new recipe">
+            <FloatButton
+              onClick={() => handleNavigate("/create")}
+              icon={<PlusOutlined />}
+            />
+          </Tooltip>
+          <Tooltip placement="right" title="Download recipe as a PDF">
+            <FloatButton icon={<DownloadOutlined />} />
+          </Tooltip>
+        </FloatButton.Group>
+        {children}
+      </main>
+    </div>
+  );
+};
 
 export default MainContent;
